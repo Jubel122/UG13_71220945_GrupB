05749b3363ee386eebedd946bd8e081e638fa490
Jubel122 <117885164+Jubel122@users.noreply.github.com>
2022-12-15 12:57:46 +0700

    Add files via upload

---
+++ Soal_GrupB_71220945.py
@@ -0,0 +1,73 @@
+import turtle 
+s = turtle.Screen()
+
+t = turtle.Turtle()
+
+#Drawing j945R
+t.forward(50)
+t.right(90)
+t.forward(70)
+t.right(90)
+t.forward(50)
+
+
+t.penup()
+t.goto(80,0)
+t.pendown()
+
+
+#Drawing 9
+t.forward(20)
+t.left(90)
+t.forward(30)
+t.left(90)
+t.forward(20)
+t.left(90)
+t.forward(30)
+t.right(180)
+t.forward(70)
+t.right(90)
+t.forward(20)
+
+
+t.penup()
+t.goto(100,0)
+t.pendown()
+
+#drawing 4
+t.right(270)
+t.forward(30)
+t.right(270)
+t.forward(30)
+t.left(90)
+t.forward(30)
+t.right(180)
+t.forward(70)
+
+t.penup()
+t.goto(170,0)
+t.pendown()
+
+
+#drawing 5
+t.right(90)
+t.forward(30)
+t.left(90)
+t.forward(30)
+t.left(90)
+t.forward(30)
+t.right(90)
+t.forward(40)
+t.right(90)
+t.forward(30)
+
+
+t.penup()
+t.goto(220,0)
+t.pendown()
+
+
+
+
+
+s.exitonclick()
